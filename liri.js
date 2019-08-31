0c3c400c48538e0c2894cabeca5598639e3d7b11
Alexander Militar <militara106@gmail.com>
2019-08-30 20:25:48 -0700

    movie this api working

--- liri.js
+++ liri.js
@@ -4,12 +4,40 @@ var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 var axios = require('axios');
 var moment = require('moment');
+var fs = require('fs');
 
 var term = process.argv[2];
 var name = process.argv.slice(3).join(' ');
 
+function doThing(){
+fs.readFile("random.txt","utf8", function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    var term = data.slice(data.indexOf('"')+1,data.lastIndexOf('"'));
+
+    console.log(str);
+});
+}
+
+// Argument Check
+switch (term) {
+    case "concert-this":
+        bandSearch();
+        break;
+    case "spotify-this-song":
+        spotifySearch();
+        break;
+    case "movie-this":
+        movieSearch();
+        break;
+    case "do-what-it-says":
+        spotifySearch();
+        break;
+}
+
 // CONCERT-THIS
-if (term == "concert-this") {
+function bandSearch() {
     var bandURL = "https://rest.bandsintown.com/artists/" + name + "/events?app_id=codingbootcamp";
 
     axios.get(bandURL).then(
@@ -24,6 +52,7 @@ if (term == "concert-this") {
                 "\nLocation: " + response.data[0].venue.city + ", " + response.data[0].venue.region +
 
                 "\nDate: " + (moment(response.data[0].datetime).format("MM/DD/YYYY")) +
+
                 "\n---------------------------------------"
             );
         }
@@ -51,7 +80,7 @@ if (term == "concert-this") {
 }
 
 // SPOTIFY-THIS-SONG
-else if (term == 'spotify-this-song') {
+function spotifySearch() {
     spotify.search({
         type: 'track',
         query: name
@@ -66,14 +95,68 @@ else if (term == 'spotify-this-song') {
 
         console.log(
             "\n---------------------------------------" +
+
             "\nArtist(s): " + artists.join(', ') +
+
             "\nTrack: " + name +
-            "\nPreview Link: " + response.tracks.items[0].album.href+
+
+            "\nPreview Link: " + response.tracks.items[0].album.href +
+
             "\nAblum: " + response.tracks.items[0].album.name +
+
             "\n---------------------------------------"
         );
     });
 }
 
 // MOVIE-THIS
-else if(term == "movie-this")
+function movieSearch() {
+    var movieURL = "http://www.omdbapi.com/?apikey=trilogy&t=" + name
+
+    axios.get(movieURL).then(
+        function (response) {
+            console.log(
+                "\n---------------------------------------" +
+
+                "\nTitle: " + name +
+
+                "\nYear: " + moment(response.data.Released).format("MM/DD/YYYY") +
+
+                "\nIMDB Rating: " + response.data.Ratings[0].Value +
+
+                "\nRotten Tomatoes: " + response.data.Ratings[1].Value +
+
+                "\nCountry: " + response.data.Country +
+
+                "\nLanguage: " + response.data.Language +
+
+                "\nPlot: " + response.data.Plot +
+
+                "\nActors: " + response.data.Actors
+
+                +
+                "\n---------------------------------------"
+            );
+        }
+    ).catch(function (error) {
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log("---------------Data---------------");
+            console.log(error.response.data);
+            console.log("---------------Status---------------");
+            console.log(error.response.status);
+            console.log("---------------Status---------------");
+            console.log(error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an object that comes back with details pertaining to the error that occurred.
+            console.log(error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+        }
+        console.log(error.config);
+    });
+
+}
